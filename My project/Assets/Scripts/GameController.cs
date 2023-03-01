aadd011367de5d3122c1761e2883c67de6a11474
German Guerero <gdguerrero@unal.edu.co>
2023-02-28 21:47:40 -0500

    :sparkles: new changes

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -80,7 +80,6 @@ public class GameController : MonoBehaviour
 
         int[,] gameState = new int[8, 8];
 
-
         for (int i = 0; i < 8; i++) for (int j = 0; j < 8; j++) {
             Debug.Log(pieces.GetTile(corner + new Vector3Int(i, j, 0))?.name);
         }
